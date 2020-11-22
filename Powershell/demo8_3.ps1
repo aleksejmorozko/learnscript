4a7de811f1c7d185a36696e21371cbd2dfefdbc5
Aleksej Morozko <aleksejmorozko@gmai.com>
2020-11-22 15:05:36 +0300

    commit_4_1

--- Powershell/demo8_3.ps1
+++ Powershell/demo8_3.ps1
@@ -1,8 +1,8 @@
 #Write a script to calculate a dog's age in human years: for the first two years, a dog year is equal to 10.5 human years. After that, each dog year equals 4 human years.
 param($s)
 if($s -le 2){
-    $s=$s*2
+    $s=$s*10.5
 }else{
-    $s=($s-1)*4
+    $s=($s-2)*4 + 21
 }
 write-output($s)
