074c3451910dbf0b5e3043cf8380c56116abc51e
AleksejMorozko <aleksejmorozko@gmail.com>
2021-01-05 00:33:45 +0300

    homeWorkComplete_1_3

--- bash_script/hworkF1.sh
+++ bash_script/hworkF1.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-cat ./accounts.csv | awk 'NR < 2' > accounts_new.csv
+#Для работы с csv файлами установим утилиту cdvtool. При помощи даннй утилиты очень удобно раскодировать (распарсить) csv файлы.
+#создаем шаблон для будущего файла.
+echo "id,location_id,organization_id,service_id,name,title,email,department" > accounts_new_mail.csv
 
-cat accounts.csv | awk -F"," 'NR>1 { print $1 "," $2 "," $3 "," $4 "," }'> ./new_csv1
-cat accounts.csv | awk -F"," 'NR>1 { print $5 }' | awk -F"[ ]" '{print toupper(substr($1,1,1)) substr($1,2)" "(toupper(substr($2,1,1)) substr($2,2))}'> ./new_csv2
-cat accounts.csv | awk -F"," 'NR>1 { print "," $6 "," }'> ./new_csv3
-# cat accounts.csv | awk -F"[,]" 'NR>1 { print $5 }' | awk -F"[ ]" '{print tolower(substr($1,1,1)) toupper(substr($2,1,1)) tolower(substr($2,2)) "@abc"}'> ./new_csv4
+#создаем шаблон без заголовка. С таким файлом проще работать.
+cat accounts.csv | awk 'NR>1 {print $0}' > accounts_1.csv
 
+#Делаем выборки из файлов по определенным полям, с которым нам проще работать.
+cat accounts_1.csv | csvtool col 1-4 - > ./new_csv1   #выборка при помощи утилиты с 1 по 4 поле и занесение данных во временный файл
+#редактирование поля содержащего имя и фамилию
+cat accounts_1.csv | awk -F, '{ print $5 }' | awk -F"[ ]" '{print ","toupper(substr($1,1,1)) substr($1,2)" "(toupper(substr($2,1,1)) substr($2,2))","}'> ./new_csv2
+#выборка оставшихся полей и занесение их в отдельный файл.
+cat accounts_1.csv | csvtool col 6 - > new_csv3
+#выбираем во временный файл оставшееся 8-е поле
+cat accounts_1.csv | awk -F"[,]" '{ print "," $8 }'> ./new_csv5
+
+#Создаем массив с шаблоном для почты для каждого пользователя
 i=0
 for name in $(cat accounts.csv | awk -F"[,]" 'NR>1 { print $5 }' | awk -F"[ ]" '{print tolower(substr($1,1,1)) tolower(substr($2,1))}'); do
   a[$i]=$name
   i=$i+1
 done
 
+#количество строк или длина массива. 
 length=${#a[@]}
+#перебираем массив и изменяем адрес при совпадении
 for (( i=0; i < $length; i++))
 do
     s=0
@@ -32,11 +44,10 @@ done
 for (( i=0; i < $length; i++ ))
 do
    a[$i]+="@abc"
-   echo ${a[$i]} >> new_csv4
+   echo ","${a[$i]} >> new_csv4
 done
 
-cat accounts.csv | awk -F"[,]" 'NR>1 { print "," $8 }'> ./new_csv5
-
+#формируем результирубющий файл и удаляем временные файлы
 paste new_csv1 new_csv2 new_csv3 new_csv4 new_csv5 > ./new_csv 
-tr -d "\t" < ./new_csv >> ./accounts_new.csv
-# rm new_csv new_csv1 new_csv2 new_csv3 new_csv4 new_csv5
+tr -d "\t" < ./new_csv >> ./accounts_new_mail.csv
+rm new_csv new_csv1 new_csv2 new_csv3 new_csv4 new_csv5 accounts_1.csv
