b2806c75813abb6befda4141e40eafd04f05c708
AleksejMorozko <aleksejmorozko@gmail.com>
2021-03-23 16:14:29 +0300

    full_ready

--- SQL/just/display_scr.sh
+++
@@ -1,1 +0,0 @@
-watch -n 3 cat ./tt.txt
--- repair.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true, HelpMessage="Enter name or IP PC")]
-    [string]$name
-)
-$user = Get-Credential #-UserName Administrator
-Invoke-Command -ScriptBlock {
-    Test-ComputerSecureChannel –Repair
-} -ComputerName $name -Credential $user
